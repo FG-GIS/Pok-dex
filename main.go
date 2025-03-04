294e4e73969c868198955a981e1c428599288e45
FG-GIS <flore.giovanni.scripting@outlook.com>
2025-03-04 03:04:53 +0100

    help/exit commands

--- main.go
+++ main.go
@@ -7,12 +7,49 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var validCommands map[string]cliCommand
+
 func cleanInput(text string) []string {
 	cleanOut := strings.Fields(strings.ToLower(text))
 	return cleanOut
 }
 
+func commandHelp() error {
+	fmt.Printf("\nWelcome to the Pokedex!\nUsage:\n\n")
+	for _, v := range validCommands {
+		fmt.Printf("%v: %v\n", v.name, v.description)
+	}
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+
+	defer os.Exit(0)
+
+	return fmt.Errorf("user interrupt - exit command")
+}
+
 func main() {
+	validCommands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Prints the list of commands",
+			callback:    commandHelp,
+		},
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("Pokedex > ")
 	for {
@@ -22,7 +59,14 @@ func main() {
 			return
 		}
 		text := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %v\n", text[0])
+		cmd, ok := validCommands[text[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+		}
+		err := cmd.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
 		fmt.Print("Pokedex > ")
 	}
 }
